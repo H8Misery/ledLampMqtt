d0ccf035a690c73f8f9471e909f7575490503441
H8Misery <foroff96@gmail.com>
2024-03-20 02:49:52 +0300

    constructor and replaced component from mainboard to app

--- src/MainControlBoard.js
+++ src/MainControlBoard.js
@@ -1,38 +1,21 @@
 import { useState, useEffect } from 'react';
 // import { createConnection } from './ws/ConnectMqtt'; //old method
-import { SwitchLamp, SendButton, SelectButton } from './Components/Commands';
-import MyInput from './Components/MyInput'
 import { connector } from './ws/mqtt_socket'
 
 export default function SelectHost () {
     // const [serverUrl, setServerUrl] = useState('ws://localhost:9001/ws');
     const [url, setServerUrl] = useState('mqtt://localhost:9001/mqtt');//хук для хоста
     const [topic, setTopic] = useState('glamp');//хук для топика (куда подпишемся)
-    const [title, setTitle] = useState(''); //хук для инпута и отправки кастомного сообщения
-    const [powerlamp, setPowerLamp] = useState(true); //хук для состояния лампы (вкл/выкл)
-    const [counter, setCounter] = useState(0); if (counter>50) {setCounter(0)};
-
-
+    
     useEffect(() => {//вызывается после монтирования компонента и каждый раз после изменения массива зависимостей
         // const connection = createConnection(url, topic); //объявили кастомный метод и отдаем серв + топик, который наследовали из App.js
         // connection.connect(); //не оч понимаю почему мы не можем сразу вызвать createConnection.connect()
-        connector.init(url, topic);
-        connector._connect();
-
-        // connector.init(topic);
-        // connector.subscribe(); //вылетает
-
-        // connector.init(topic);
-        connector.sniff(); //лагает, думаю это нужно в коннекторе вызывать
-        // connect._connect()
+        // connector.init(url, topic);
+        connector.init(url, topic)
+        connector.sniff();
         return () => { //это функция очистки которая вызывается перед следующим срабатыванием эффекта и последний раз после размонтирования компонента
-            // connection.disconnect();//
-        connector._disconnect(); //#2
-        // connector.init(topic);
-        // connector.publishMessage('PC WANT TO CONNECT');
 
-        // disconnection.disconnect();
-        
+            connector.disconnect();
         };
     }, [url, topic]); //массив зависимостей
 //Ниже мы рендерим форму при монтировании объекта в App.js для того, чтобы выбрать нужный хост и топик
@@ -58,31 +41,6 @@ export default function SelectHost () {
                 <option value="glamp">Env</option>
                 <option value="LedLamp/LedLamp_...">Prod</option>
             </select>
-        <SwitchLamp
-            url={url}
-            topic={topic}
-            value={powerlamp ? 'P_ON' : 'P_OFF'}
-            onChange={() => setPowerLamp(!powerlamp)}
-            type="checkbox"
-            />{!powerlamp ? 'ON' : 'OFF'}
-        <MyInput
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            type="text"
-            placeholder="Введите команду"/>
-        <SendButton
-            url={url}
-            topic={topic}
-          // {...title==='' ? '' : 'disabled'}
-            value={title}>Send
-        </SendButton>
-        <SelectButton
-            url={url}
-            topic={topic}
-            onWheel={() => setCounter (counter+1)}
-            value={`EFF${counter}`}>{`Эффект №${counter}`}
-        </SelectButton>
-
     </div>
     )
 }
--- src/ws/mqtt_socket.js
+++ src/ws/mqtt_socket.js
@@ -1,14 +1,12 @@
 import mqtt from 'mqtt';
 
-
-
-  class MqttConnector {
+class MqttConnector {
     
-    init(url, topic) {
-        console.log(`Init have host: '${url}'\nTopic '${topic}'`)
+    constructor(url, topic) {
+        console.log(`Constructor have host: '${url}'\nand Topic '${topic}'`)
         this.url = url;
-        this.socket = null
         this.topic = topic
+        this.socket = null //обнуляем сокет по умолчанию, чтобы не плодить коннекты к хосту
         this.options = {
             // Clean session
             clean: true,
@@ -18,79 +16,68 @@ import mqtt from 'mqtt';
             // clientId: 'emqx_test',
             // username: 'emqx_test',
             // password: 'emqx_test',
-
           }
-        //   console.log(`Socket is ${this.socket}`)
     }
 
-    _connect() {
-        if(!this.socket) {
-            console.log(`Trying to connect ${this.url} ... `)
-            this.socket = mqtt.connect(this.url, this.options)
-            
-            this.socket.on("connect", () => {
-                console.log(`✅ Connected to ${this.url}`)
-                this.subscribe();
-            this.socket.on("reconnect", () => {
-                console.log('RECONNECTING ... ... ...')
-            })
-            });
-            // this.socket.publish(this.topic, 'asd')
+    init(newUrl, newTopic) {
+        console.log(`init have host: '${newUrl}'\nand Topic '${newTopic}'`)
+        this.url=newUrl;
+        this.topic=newTopic;
+        this.socket = null //обнуляем сокет при изменении, чтобы не плодить коннекты к хосту
 
-            // this.socket.on("close", (err) => {
-            //     console.log("Disconnected");
-            //     console.log(err)
-            // });
-            
-        } else {
-            
-            }
-            // console.log(`Socket is ${this.socket.connected}`) 
-        return ( 
-            this.socket
-            )
+        console.log(`init changed host: '${this.url}'\nand Topic '${this.topic}'`)
     }
 
-    subscribe() {//тестово вызываю сразу после коннекта
-        const socket = this._connect();
-        socket.subscribe(this.topic, { qos: 0 }, (err) => {
-            console.log(`✅ Subscribed to ${this.topic}`)
-                // if (!err) {
-                //     this.socket.publish(this.topic, `PC is connected to MQTT on ${this.topic}`)
-                // } else {
-                //     console.log(err)
-                // }
-        
-        });
-        this.sniff();//если подписан - нюхаем топик V
-    }
     sniff() {//тестово сразу вызываю после подписки ^
         // this.subscribe();
         const socket = this._connect();
         socket.on("message", function (topic, received) {
-        console.log(`Received in mqtt_socket.connect \n MSG: ${received} \n TOPIC: ${topic}`);
+        console.log(`Received in sniffer: \n MSG: ${received} \n URL: ${this.url} \n TOPIC: ${topic}`);
         })
     }
-    _disconnect() {
-        console.log('Disconnection')
-        this.socket.end();
-        
-        this.socket.on("end", () => {
-            console.log(`Disconnected from ${this.url}`)
-        });
-        // this.socket = null
+
+    _connect(){
+        if(!this.socket) {
+            console.log(`Trying to connect ${this.url} ... `)
+            this.socket = mqtt.connect(this.url, this.options);
+            
+            this.socket.on("connect", () => {
+                console.log(`✅ Connected to ${this.url}`);
+
+            this.socket.subscribe(this.topic, { qos: 0 }, (err) => {
+                err ? console.log(err) : console.log(`✅ Subscribed to ${this.topic}`)
+                });
+            })
+
+            this.socket.on("end", () => {
+                console.log(`Disconnected from ${this.url}`)
+            });
+
+            this.socket.on("reconnect", () => {
+                console.log('RECONNECTING ... ... ...');
+            });
+            // this.socket.end();//потом придумаю как вызывать сокет энд, и окончательно избавиться от disconnect(); который вызываю при очистке в useEffect, в MainControlBoard
+        } return this.socket
+    }
+
+    disconnect() {
+        // const socket = this._connect();
+        console.log('Disconnection...')
+        const socket = this._connect();
+        socket.end();
+        this.socket.on("close", (err) => {
+            err ? console.log(err) : console.log('Closing connection')
+            });
+
     }
     publishMessage(message) {
         const socket = this._connect();
         socket.publish(this.topic, message, { qos: 0, retain: false }, function (err) {
-            if (err) {
-            console.log(this.topic)
-            console.log(message)
-            console.log(err)
-            }
+            err ? console.log(err) : console.log('Published')
         });
-    }
     
-}
+    }
 
-export const connector = new MqttConnector();
+
+}
+export const connector = new MqttConnector('mqtt://localhost:9001/mqtt', 'glamp');
